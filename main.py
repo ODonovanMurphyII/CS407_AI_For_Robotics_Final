e1771f969852275c10f82766abe2103696841606
Don Murphy <dm4224@gmail.com>
2025-12-07 17:07:23 -0500

    Updated main.py to correct version and moved parameter file to main directory. Results from testing can be found in test1

--- main.py
+++ main.py
@@ -2,70 +2,62 @@ import easygopigo3 as easy
 from ultralytics import YOLO
 import cv2 as cv 
 import time
+import os
+
 
 ## Creating Camera Object
-cam = cv2.VideoCapture(0)
+cameraIndex = 0
+cam = cv.VideoCapture(cameraIndex)
+if(not cam.isOpened()):
+    print("Could not open the camera")
+else:
+    print("Camera Connected")
+    ## Waking the camera up
+    for i in range(25):
+        cam.read()
+        time.sleep(0.1)
 
-## Waking the camera up
-for i in range(25):
-    cam.read()
-    time.sleep(0.1)
 
 ## need to move this somewhere better but for now
-myModel = YOLO("yolov8n.pt")
-vehicles = 0
-bad_guys = 1
+myModel = YOLO("best.pt")
+VEHICALS = 0
+BAD_GUYS = 1
+THRESHOLD = 0.30
 
-def explore(EasyGoPiGo3: robot):
+def explore():
     i = 0
-    headingList[i]
     while(i < 360):
+        print("Taking image")
+        allClearFileName = os.path.join("./clear", "image" + str(i) + ".jpeg")
+        badGuysFileName = os.path.join("./notclear", "image" + str(i) + ".jpeg")
         ret, image = cam.read()
-        if not ret:
-            ret, image = cam.read()
+        result = myModel.predict(source=image, save=True, classes=[BAD_GUYS])
+        confidence = result[0].boxes.conf[0].item() if result[0].boxes else 0
+        if(confidence > THRESHOLD):
+            print("BAD GUY!\n")
+            cv.imwrite(badGuysFileName, image)
+            print("Image saved: " + badGuysFileName + "\n")
         else:
-            headingList[i] = myModel.predict(source=image, save=True, classes=[] )
+            print("ALL CLEAR!\n")
+            cv.imwrite(allClearFileName, image)
+            print("Image saved: " + allClearFileName + "\n")
         i += 1
-        robot.turn_degrees(i)
-    return headingListUp, headingListDown, headingListLeft, headingListRight
-
-# must be run IMMEDIATELY after explore
-def take_action(upResults, downResults, leftResults, rightResults, EasyGoPiGo3: robot):
-    upMax = max(upResults)
-    upHeading = upResults.index(upMax)
-
-    downMax = max(downResults)
-    downHeading = downResults.index(downMax)
-
-    leftMax = max(leftResults)
-    leftHeading = leftResults.index(leftMax)
-
-    rightMax = max(rightResults)
-    rightHeading = rightResults.index(rightMax)
-
-    largest = max(upMax, downMax, leftMax, rightMax)
-    if largest == upMax:
-        robot.turn_degrees(upHeading)
-    elif largest == downMax:
-        robot.turn_degrees(downHeading)
-    elif largest == leftMax:
-        robot.turn_degrees(leftHeading)
-    elif largest == rightMax:
-        robot.turn_degrees(rightHeading)
+        #robot.turn_degrees(i)
 
 def main():
     # Objects
-    robot = easy.EasyGoPiGo3()
-    proxSensor = robot.init_distance_sensor()
-    frontServo = robot.init_servo("SERV01")
+    #robot = easy.EasyGoPiGo3()
+    #proxSensor = robot.init_distance_sensor()
+    #frontServo = robot.init_servo("SERV01")
 
     #inits 
-    robot.stop()
-    robot.set_speed(200)
+    #robot.stop()
+    #robot.set_speed(200)
 
-    #Starting our maze solving loop
-    # TODO this is crude but gets the basic idea
-    while(1):
+    print("Bad Guy Identifier!\n")
+    print(myModel.names)
+    #while(1):
+    explore()
         
 
 
